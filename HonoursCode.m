ae1430fe98d2e1d5225fe7e99edeb2c989d32c61
corneyr <corneyr@utas.edu.au>
2023-05-08 12:58:01 +1000

    HonoursCode.m

--- HonoursCode.m
+++ HonoursCode.m
@@ -1,10 +1,10 @@
 %%%%% import Characteristics_redcap.csv file
 %Characteristics_redcap 
 pathdef = 'C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data'
-addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data')
+addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data');
 filename = 'characteristics_redcap.csv';
 
-file = sprintf('%s\\%s',pathdef, filename)
+file = sprintf('%s\\%s',pathdef, filename);
 
 % readtable('characteristics_redcap.csv')
 
@@ -21,10 +21,10 @@ file = sprintf('%s\\%s',pathdef, filename)
 data = readtable(file);
 
 % select the column containing the XML file names
-file_column = data(:, 2)
-file_column = data(:, 4)
-file_column = data(:, 17)
-file_column = data(:, 20)
+file_column = data(:, 2);
+file_column = data(:, 4);
+file_column = data(:, 17);
+file_column = data(:, 20);
 
 %file_column = data(:, 2), data(:, 4), data(:, 17), data(:, 20)
 
@@ -38,10 +38,10 @@ end
 
 
 %%%BPplus data - not needed
-addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\Hobart BP+\0000')
-DOMnode = xmlread('BPplus_00001.xml')
-readtable('BPplus_00001.xml')
-xmlread('BPplus_00001.xml')
+addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\Hobart BP+\0000');
+DOMnode = xmlread('BPplus_00001.xml');
+readtable('BPplus_00001.xml');
+xmlread('BPplus_00001.xml');
 
 %%%BPplus data - not needed
 
@@ -84,25 +84,29 @@ for i = 1:childNodes.getLength()
 end
 
 %%%Reading in the xml files
-[fn, path] = uigetfile()
+[fn, path] = uigetfile();
 
 %%%Plotting the xml file
 %cuffPressure = ReadXML('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\Hobart BP+\0000/0000/BPplus_00001.xml')
 cuffPressure = xmlread([path fn], 'AllowDoctype',true);
-getFirstChild(cuffPressure)
-cuffPressure = ReadXML([path fn])
-plot(cuffPressure)
+getFirstChild(cuffPressure);
+cuffPressure = ReadXML([path fn]);
+plot(cuffPressure);
 %%%Plotting the xml file
 
 sampletime = 0.004;
 osc = Oscillogram(cuffPressure, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 1);
-set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+%set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
 
-[beatI, regionLimits] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1)
+%%%beat detection%%%
+[beatI, regionLimits] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1);
 
 %%%pathway to invasive files%%
-addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\InvasiveBP')
+addpath('C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\InvasiveBP\');
 %%%pathway to invasive files%%
 
+%%Plot invasive data%%
 readtable('100176076_22062022_cuff1_P.txt')
-
+invasivedata = readtable('100176076_22062022_cuff1_P.txt');
+plot(invasivedata.Var1);
+%%Plot invasive data%%
