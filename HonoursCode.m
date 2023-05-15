f0aa32eca60bc100eb9ee5f2c435c015d1cfc471
corneyr <corneyr@utas.edu.au>
2023-05-15 12:01:03 +1000

    HonoursCode.m

--- HonoursCode.m
+++ HonoursCode.m
@@ -2,16 +2,37 @@
 characteristicsredcap = getCharacteristics_redcap;
 %%
 
-%% getXMLfile from cvs file
+%% get xml name from files csv file
 % xml_data = getXMLfile(file_index, toppath);
-xml_data = getXMLfile(43);  
+xml_data = getXMLfile(23);  
 %%
 
 %% plotting the cuff BP xml data 
+% may not be needed anymore
 xmldatafigure = plotxmldata;
 %%
 
 %% pathway to invasive files
 % plot invasvie files in mmHg
 invasivefigure = plotinvasive;
+%%
+
+%% Finding xml files and producing the figures
+outerDirectoryName = "C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\Hobart BP+";
+
+outerDirectory = dir(outerDirectoryName);
+
+for i = 1:length(outerDirectory)
+    if outerDirectory(i).name ~= "." && outerDirectory(i).name ~= ".."
+        %We should check that these are directories just in case someone
+        %slips in a file into Hobart BP+
+        innerDirectory = dir(outerDirectoryName  + "\" + outerDirectory(i).name);
+        for j = 1:length(innerDirectory)
+            if ~innerDirectory(j).isdir
+                path = outerDirectoryName  + "\" + outerDirectory(i).name + "\" + innerDirectory(j).name;
+                plotxmldata(path);
+            end
+        end
+    end
+end
 %%
