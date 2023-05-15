c7e41895964ecaa8064bf22e2252e9667d98db58
corneyr <corneyr@utas.edu.au>
2023-05-15 12:02:49 +1000

    plotxmldata.m

--- plotxmldata.m
+++ plotxmldata.m
@@ -1,25 +1,31 @@
-function xmldatafigure = plotxmldata;
+function xmldatafigure = plotxmldata(path)
 
     % finds xml data pathway
     % reads xml data 
     % plots xml data in mmHg
 
+    disp(nargin)
+
+if nargin == 0
 % xml files pathway
-[fn, path] = uigetfile();
+    [fn, path] = uigetfile();
+
+    pathFile = [path fn];
+else
+    pathFile = path;
+end
 
 % Plotting the xml file
-cuffPressure = xmlread([path fn], 'AllowDoctype',true);
+cuffPressure = xmlread(pathFile, 'AllowDoctype',true);
 getFirstChild(cuffPressure);
-cuffPressure = ReadXML([path fn]);
-plot(cuffPressure);
+cuffPressure = ReadXML(pathFile);
 plotxmldata = plot(cuffPressure);
-
 xmldatafigure = plotxmldata;
 
 % plotting cuff BP as an oscillogram
 sampletime = 0.004;
 osc = Oscillogram(cuffPressure, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 1);
-% set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+ %set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
 
 % beat detection of the oscillogram
 [~,maxI] = max(cuffPressure);
