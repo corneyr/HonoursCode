872e1d4fb171304f07f03d56a6860569775d357a
corneyr <corneyr@utas.edu.au>
2023-05-18 11:48:11 +1000

    plotinvasive.n

--- plotinvasive.m
+++ plotinvasive.m
@@ -1,16 +1,16 @@
-function invasivefigure = plotinvasive
+function invasivefigure = plotinvasive(path_file)
 % pathway to invasive files
 if ~exist('pathdef', 'var')
-    pathdef = 'C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\InvasiveBP\';
+    path_file = 'C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\InvasiveBP\';
 end
 
 % pathway to invasive files
 
-filename = sprintf('%09_%08_cuff%01_P.txt');
+%filename = sprintf('%09_%08_cuff%01_P.txt');
 
 % Plot invasive data in mmHg
-readtable('100176076_22062022_cuff1_P.txt')
-invasivedata = readtable('100176076_22062022_cuff1_P.txt');
+%readtable('100176076_22062022_cuff1_P.txt')
+invasivedata = readtable(path_file);
 plot(invasivedata.Var1);
 
 invasivefigure = plot(invasivedata.Var1);
