f108ed2cb37c5c33734bb16b7d0490e3be0ab82f
corneyr <corneyr@utas.edu.au>
2023-06-01 12:54:23 +1000

    plotinvasive.m

--- plotinvasive.m
+++ plotinvasive.m
@@ -15,9 +15,12 @@ end
 %filename = sprintf('%09_%08_cuff%01_P.txt');
 
 % Plot invasive data in mmHg
+readtable(pathFile);
 invasivedata = readtable(pathFile);
-
+figure()
 invasivefigure = plot(invasivedata.Var1);
-drawnow
+% drawnow
+
+
 
 end
