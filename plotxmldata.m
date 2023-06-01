edd01ed4f3f96d0b9afc4d7a895b4be6fbf81344
corneyr <corneyr@utas.edu.au>
2023-06-01 12:54:00 +1000

    plotxmldata.m

--- plotxmldata.m
+++ plotxmldata.m
@@ -17,17 +17,21 @@ end
 
 % Plotting the xml file
 cuffPressure = ReadXML(pathFile);
+figure()
 plotxmldata = plot(cuffPressure);
 xmldatafigure = plotxmldata;
+ set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+
 
 % plotting cuff BP as an oscillogram
 sampletime = 0.004;
 osc = Oscillogram(cuffPressure, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 1);
- %set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+    set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
 
 % beat detection of the oscillogram
-[~,maxI] = max(cuffPressure);
 [beatI, regionLimits] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1, ...
-                            'RegionLimits', [maxI, length(cuffPressure)], 'MinimumThreshold', 0.3, 'DerivativePeakThreshold', 0.05);
+                            'RegionLimits', [max(cuffPressure), length(cuffPressure)], 'MinimumThreshold', 0.3, 'DerivativePeakThreshold', 0.1);
+    % set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+
 
 end
