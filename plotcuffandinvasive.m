d80c75ebd991af608cb51988525d44cdb24c2f04
corneyr <corneyr@utas.edu.au>
2023-06-01 12:55:38 +1000

    plotcuffandinvasive.m

--- plotcuffandinvasive.m
+++ plotcuffandinvasive.m
@@ -12,7 +12,16 @@ plot(bigData(2).cuffdata{1});
 subplot(2,1,1);
 plot(bigData(1).invasivedata{2});
 subplot(2,1,2);
-plot(bigData(2).cuffdata{2});
+plot(bigData(1).cuffdata{2});
 
 
-end
+% with parameters 
+figure()
+subplot (2,1,1);
+plot(diff(bigData(18).cuffdata{1,1}(1890:6290)))
+subplot (2,1,2);
+plot(bigData(18).invasivedata{1,1});
+
+
+
+   
