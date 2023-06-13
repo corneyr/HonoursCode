6ab6b8ccf4a82400899b8b94a863e707840236b4
corneyr <corneyr@utas.edu.au>
2023-06-13 10:49:38 +1000

    linkingxmltotxt.m

--- linkingxmltotxt.m
+++ linkingxmltotxt.m
@@ -14,10 +14,10 @@ for i = 1:height(characteristicsredcap)
     id = characteristicsredcap.thci(i); %for pointing to invasive recording
     cuffID1 = characteristicsredcap.measno_aor1(i); %for pointing to cuff1
     cuffID2 = characteristicsredcap.measno_aor2(i); %for pointing to cuff2
-    cuffIdxs_S1 = characteristicsredcap.startI_1(i); %need to check
-    cuffIdxs_E1 = characteristicsredcap.endI_1(i); %need to check
-    cuffIdxs_S2 = characteristicsredcap.startI_2(i); %need to check
-    cuffIdxs_E2 = characteristicsredcap.endI_2(i); %need to check
+    cuffIdxs_S1 = characteristicsredcap.startI_1(i); %for pointing at start index 1
+    cuffIdxs_E1 = characteristicsredcap.endI_1(i); %for pointing at end index 1
+    cuffIdxs_S2 = characteristicsredcap.startI_2(i); %for pointing at start index 2
+    cuffIdxs_E2 = characteristicsredcap.endI_2(i); %for pointing at end index 1
 
     bigData(i).id = id;
     bigData(i).cuffID = {};
@@ -25,13 +25,12 @@ for i = 1:height(characteristicsredcap)
     bigData(i).invasive_path = {};
     bigData(i).cuffID{1} = cuffID1;
     bigData(i).cuffID{2} = cuffID2;
-    bigData(i).cuffIdxs = {};
-    bigData(i).cuffIdxs{1} = {}; 
-    bigData(i).cuffIdxs{2} = {};
-    bigData(i).cuffIdxs{1} = cuffIdxs_S1; %start of deflation cuff1
-    bigData(i).cuffIdxs{2} = cuffIdxs_E1; %end of deflation cuff1
-    bigData(i).cuffIdxs{3} = cuffIdxs_S2; %start of deflation cuff2
-    bigData(i).cuffIdxs{4} = cuffIdxs_E2; %end of deflation cuff2
+    bigData(i).cuffIdxs1 = {};
+    bigData(i).cuffIdxs1{1} = cuffIdxs_S1; %start of deflation cuff1
+    bigData(i).cuffIdxs1{2} = cuffIdxs_E1; %end of deflation cuff1
+    bigData(i).cuffIdxs2 = {};
+    bigData(i).cuffIdxs2{1} = cuffIdxs_S2; %start of deflation cuff2
+    bigData(i).cuffIdxs2{2} = cuffIdxs_E2; %end of deflation cuff2
 
     if ~isnan(cuffID1) %Check if Cuff recording exists
         two_files = dir(home_path +"\InvasiveBP\" + id + "*"); %the two invasive .txts
