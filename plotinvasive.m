5a190592c0aff7fbb750639aa121bdd3414478a4
corneyr <corneyr@utas.edu.au>
2023-06-20 12:43:35 +1000

    plotinvasive.m

--- plotinvasive.m
+++ plotinvasive.m
@@ -4,23 +4,30 @@ function invasivefigure = plotinvasive(path)
 
 if nargin == 0
 % xml files pathway
-    [fn, path] = uigetfile();
+    [fn, path] = uigetfile("*.*");
 
     pathFile = [path fn];
 else
     pathFile = path;
 end
-% pathway to invasive files
-
-%filename = sprintf('%09_%08_cuff%01_P.txt');
 
 % Plot invasive data in mmHg
-readtable(pathFile);
+%readtable(pathFile);
 invasivedata = readtable(pathFile);
 figure()
 invasivefigure = plot(invasivedata.Var1);
-% drawnow
+% ax = gca;
+% exportgraphics(ax, 'invasiveplot.jpg')
 
+[beatI, regionLimits] = analysis.BeatOnsetDetect(invasivedata.Var1, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(invasivedata.Var1), length(invasivedata.Var1)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
 
-
+beatI = round(beatI);
+for n = 1:length(beatI)
+    starti = beatI(n);
+    endi = beatI(n+1);
+    pulse = invasivedata.Var1(starti:endi);
+    time = (0:(length(pulse)-1))*0.001;
+    [AI, Paug, featuretimes] = AugmentationIndex(time, pulse, 'DoPlot',1);
+end 
 end
+
