72f0d91bcb6260130bd98245183f62d2eecc67ab
corneyr <corneyr@utas.edu.au>
2023-06-20 12:43:58 +1000

    plotxmldata.m

--- plotxmldata.m
+++ plotxmldata.m
@@ -8,28 +8,34 @@ function xmldatafigure = plotxmldata(path)
 
 if nargin == 0
 % xml files pathway
-    [fn, path] = uigetfile();
+    [fn, path] = uigetfile("*.*");
 
     pathFile = [path fn];
 else
     pathFile = path;
 end
 
+
 % Plotting the xml file
 cuffPressure = ReadXML(pathFile);
 figure()
 plotxmldata = plot(cuffPressure);
+ax = gca;
+exportgraphics(ax, 'cuffplot.jpg')
 xmldatafigure = plotxmldata;
-    set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+    % set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
 
 % plotting cuff BP as an oscillogram
 sampletime = 0.004;
 osc = Oscillogram(cuffPressure, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 1);
-   set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
+ax = gca;
+exportgraphics(ax, 'oscplot.jpg')
+   % set(gcf, 'position', 1.0e+03 * [-1.6663    0.2143    1.1560    0.5073]);
 
 % beat detection of the oscillogram
 [beatI, regionLimits] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(cuffPressure), length(cuffPressure)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.05);
-
+ax = gca;
+exportgraphics(ax, 'beatdetectionplot.jpg')
 
 beatI = round(beatI);
 dt = 1/200;
@@ -39,7 +45,7 @@ for n = 1:length(beatI)
         endi = beatI(n+1);
         pulse = osc(starti:endi);
         time = (0:(length(pulse)-1))*dt;
-        [AI, Paug, featuretimes] = AugmentationIndex(time, pulse, 'DoPlot',1);  
+        [AI, Paug, featuretimes] = AugmentationIndex(time, pulse, 'DoPlot',1); 
 end
 
 end
