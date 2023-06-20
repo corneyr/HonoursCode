ce44336008043dde30bcd5a91f0d374a68278977
corneyr <corneyr@utas.edu.au>
2023-06-20 12:46:48 +1000

    plotinvasive.m

--- plotinvasive.m
+++ plotinvasive.m
@@ -21,13 +21,13 @@ invasivefigure = plot(invasivedata.Var1);
 
 [beatI, regionLimits] = analysis.BeatOnsetDetect(invasivedata.Var1, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(invasivedata.Var1), length(invasivedata.Var1)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
 
-beatI = round(beatI);
-for n = 1:length(beatI)
-    starti = beatI(n);
-    endi = beatI(n+1);
-    pulse = invasivedata.Var1(starti:endi);
-    time = (0:(length(pulse)-1))*0.001;
-    [AI, Paug, featuretimes] = AugmentationIndex(time, pulse, 'DoPlot',1);
-end 
+    beatI = round(beatI);
+    for n = 1:length(beatI)
+        starti = beatI(n);
+        endi = beatI(n+1);
+        pulse = invasivedata.Var1(starti:endi);
+        time = (0:(length(pulse)-1))*0.001;
+        [AI, Paug, featuretimes] = AugmentationIndex(time, pulse, 'DoPlot',1);
+    end 
 end
 
