a21d30ce00b59cccfe7718a52b9ee7c1b3baa6f3
corneyr <corneyr@utas.edu.au>
2023-06-21 12:35:00 +1000

    iseven.m

---
+++ iseven.m
@@ -0,0 +1,10 @@
+function evenness = iseven(input)
+%ISEVEN decides whether numberic input is even
+    if mod(input,2)
+        evenness = false;
+    else
+        evenness = true;
+    end
+end
+
+u
