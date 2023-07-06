c7a43cf14cbc0b523e93d76b83da8e206011828b
corneyr <corneyr@utas.edu.au>
2023-07-06 12:12:38 +1000

    compare_invasive_cuff.m

--- compare_invasive_cuff.m
+++ compare_invasive_cuff.m
@@ -1,10 +1,10 @@
 
-for i = 1:length(bigData)
-    if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) || isempty(bigData(i).invasivebrachialdata)
-        continue
-    end
+% for i = 1:length(bigData)
+%     if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) || isempty(bigData(i).invasivebrachialdata)
+%         continue
+%     end
 
-% i = 4;
+i = 7;
 
 % Note to self, we probably want to rewrite this back into bigData at some
 % point
@@ -26,11 +26,13 @@ t.String = "Brachial " + t.String;
 
 for n = 1:(length(cuff_beatI)-1)
     subplot(num_rows, num_cols, num_cols+n);
-    plot_aug_idx(bigData(i).detrend_cuffdata{1,2}(cuff_beatI(n):cuff_beatI(n+1)), 0.02); 
-    % xlim([0, 1]) %shrinks invasive to 1second
+    time_offset = length(bigData(i).cuffdata{1,2}) - length(bigData(i).detrend_cuffdata{1,2});
+    pressure_offset = bigData(i).cuffdata{1,2}(cuff_beatI(n)+time_offset);
+    data = bigData(i).detrend_cuffdata{1,2}(cuff_beatI(n):cuff_beatI(n+1)) + pressure_offset;
+    plot_aug_idx(data, 0.005); 
 end
 
-end %used for 'for i = 1:length(bigData)'
+% end %used for 'for i = 1:length(bigData)'
 
 % Labelling the axis with name and AIx for each pulse
 function t = plot_aug_idx(pulse, dt)
@@ -68,18 +70,20 @@ function cuff_beatI = calc_cuff_beati(bigData, subject)
     sampletime = 0.004;
     osc = Oscillogram(bigData(subject).detrend_cuffdata{1,2}, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 0);
     [cuff_beatI, ~] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).detrend_cuffdata{1,2}), length(bigData(subject).detrend_cuffdata{1,2})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.05);
-    cuff_beatI = round(cuff_beatI); 
+    %max(bigData(subject).detrend_cuffdata{1,2})
+
+    cuff_beatI =  round(cuff_beatI); 
 end
 
 % Detect invasive aortic average 
 function invasive_average = calc_invasive_average(bigData, subject)
-    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).invasivedata{1,2}, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).invasivedata{1,2}), length(bigData(subject).invasivedata{1,2})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
-    invasive_average = analysis.AverageBeat(bigData(subject).invasivedata{1,2}', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, 0 for no plot, 1 for show plot 
+    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).filtered_invasivedata{1,2}, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).filtered_invasivedata{1,2}), length(bigData(subject).filtered_invasivedata{1,2})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
+    invasive_average = analysis.AverageBeat(bigData(subject).filtered_invasivedata{1,2}', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, 0 for no plot, 1 for show plot 
 end
 
 % Detect invasive brachial average 
 function invasive_brachial_average = calc_invasive_brachial_average(bigData, subject)
-    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).invasivebrachialdata, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).invasivebrachialdata), length(bigData(subject).invasivebrachialdata)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
-    invasive_brachial_average = analysis.AverageBeat(bigData(subject).invasivebrachialdata', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, change to 0 for no plot
+    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).filtered_invasivebrachialdata, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).filtered_invasivebrachialdata), length(bigData(subject).filtered_invasivebrachialdata)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
+    invasive_brachial_average = analysis.AverageBeat(bigData(subject).filtered_invasivebrachialdata', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, change to 0 for no plot
 end
 
