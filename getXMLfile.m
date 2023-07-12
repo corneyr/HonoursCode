c60b84c6e6d81a34fe4a93fd64c2d61e2a29f3b1
corneyr <corneyr@utas.edu.au>
2023-07-12 17:19:49 +1000

    getXMLfile.m

--- getXMLfile.m
+++ getXMLfile.m
@@ -10,8 +10,8 @@ function [xml_data, xml_path] = getXMLfile(file_index, toppath)
 if ~exist('toppath','var')
     toppath = 'C:\Users\corneyr\OneDrive - University of Tasmania\Honours 2023\HonoursCode\Data\Hobart BP+';
 end
-folderi = [1,20,39,59,80,100,117,140,160]; %First file in each folder.As files are in numeric order, if a files name is between two values in this list then the file is in the first folder in that range
-foldern = 0:8;
+folderi = [1,20,39,59,80,100,117,140,160,180]; %First file in each folder.As files are in numeric order, if a files name is between two values in this list then the file is in the first folder in that range
+foldern = 0:9;
 
 %file_column = [2	17	21	25	NaN	30	32	34	36	38	40	42	44	46	48	50	54	56	58	60	63	65	67	69	71	73	75	79	81	83	85	87	89	NaN	91	93	95	97	99	101	103	106	108	110	112	114	116	118	120	122	124	NaN	126	128	130	133	136	139	141	143	146	149	152	157	160	NaN	167	169	171	173	175	NaN];
 
