ffc2b439c94d5f1d786841df0d91b5f32cef2282
corneyr <corneyr@utas.edu.au>
2023-07-23 11:40:40 +1000

    HonoursCode.m

--- HonoursCode.m
+++ HonoursCode.m
@@ -1,51 +1,67 @@
-% import Characteristics_redcap file and pull out thci, date_of_study, cuffbp1, cuffbp2
-% may not be needed being used for the bigData function instead
+% input: bigData table from linkningxmltotxt.m
+% input: getCharacteristics_redcap.m
+% input: directory to data on device
+% input: getXMLfile.m
+
+% This function improts data from characteristics_redcap file 
+% Finds coloums with BPplus file names, THCI, and Date of study 
 characteristicsredcap = getCharacteristics_redcap();
-%
 
 % get xml name from files csv file
 % bigdata is finding the xml and txt file names from the redcap csv file.
 bigData = linkingxmltotxt();
 
-
-% plotting cuff pulses, aortic and brachial average AIx
-% use script: compare_invasive_cuff
+% script: compare_invasive_cuff.m
 for i = 1:length(bigData)
-% i = 1;
+    if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) 
+        continue
+    end 
 
 % Note to self, we probably want to rewrite this back into bigData at some
 % point
 invasive_average = calc_invasive_average(bigData, i);
-invasive_brachial_average = calc_invasive_brachial_average(bigData, i);
-cuff_beatI = calc_cuff_beati(bigData, i);
+
+if ~isempty(bigData(i).invasivebrachialdata) 
+    invasive_brachial_average = calc_invasive_brachial_average(bigData, i);
+end
+    cuff_beatI = calc_cuff_beati(bigData, i);
 
 % plotting the aortic, brachial and cuff on one plot
 num_cols = 5;
 num_rows = ceil((length(cuff_beatI)-1)/num_cols)+1;
 
 subplot(num_rows, num_cols, 1);
-t = plot_aug_idx(invasive_average, 0.001);
+[t, AI_aortic] = plot_aug_idx(invasive_average, 0.001);
 t.String = "Aortic " + t.String;
 
-subplot(num_rows, num_cols, num_cols);
-t = plot_aug_idx(invasive_brachial_average, 0.001);
-t.String = "Brachial " + t.String;
+if ~isempty(bigData(i).invasivebrachialdata) 
+    subplot(num_rows, num_cols, num_cols);
+    [t, AI_brachial] = plot_aug_idx(invasive_brachial_average, 0.001);
+    t.String = "Brachial " + t.String;
+end
 
+AIx = zeros(length(cuff_beatI)-1, 1);
 for n = 1:(length(cuff_beatI)-1)
     subplot(num_rows, num_cols, num_cols+n);
-    plot_aug_idx(bigData(i).cuffdata{1,2}(cuff_beatI(n):cuff_beatI(n+1)), 0.02); 
-    % xlim([0, 1]) %shrinks invasive to 1second
+    time_offset = length(bigData(i).cuffdata{1,1}) - length(bigData(i).filtered_cuffdata{1,1});
+    pressure_offset = bigData(i).cuffdata{1,1}(cuff_beatI(n)+time_offset);
+    data = bigData(i).filtered_cuffdata{1,1}(cuff_beatI(n):cuff_beatI(n+1)) + pressure_offset;
+    [~, AIx(n)] = plot_aug_idx(data, 0.005); 
 end
+
+bigData(i).AI_aortic = AI_aortic;
+bigData(i).AI_brachial = AI_brachial;
+bigData(i).AIx = AIx;
 
 end %used for 'for i = 1:length(bigData)'
 
 % Labelling the axis with name and AIx for each pulse
-function t = plot_aug_idx(pulse, dt)
+function [t, AI] = plot_aug_idx(pulse, dt)
     time = (0:(length(pulse)-1))*dt;
     [AI, ~, featuretimes] = analysis.AugmentationIndex(time, pulse, 'DoPlot',0);
 
     plot(time, pulse);
-    
+    t=0;
     if isstruct(featuretimes)
 
         x1 = xline(featuretimes.maxP,'-','Max P');
@@ -61,6 +77,10 @@ function t = plot_aug_idx(pulse, dt)
         x3.LabelVerticalAlignment = 'middle';
         x3.LabelHorizontalAlignment = 'center';
 
+        x1.FontSize = 6;
+        x2.FontSize = 6;
+        x3.FontSize = 6;
+
         t = title(['AI = ', num2str(AI)]);
     end
 
@@ -69,19 +89,20 @@ end
 % Detect cuff pulses
 function cuff_beatI = calc_cuff_beati(bigData, subject)
     sampletime = 0.004;
-    osc = Oscillogram(bigData(subject).cuffdata{1,2}, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 0);
-    [cuff_beatI, ~] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).cuffdata{1,2}), length(bigData(subject).cuffdata{1,2})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.05);
-    cuff_beatI = round(cuff_beatI); 
+    osc = Oscillogram(bigData(subject).filtered_cuffdata{1,1}, sampletime, 'BaselineSmoothTime', 4, 'OscillogramSmoothTime', 0.2, 'Plot', 0);
+    [cuff_beatI, ~] = analysis.BeatOnsetDetect(osc, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).filtered_cuffdata{1,1}), length(bigData(subject).filtered_cuffdata{1,1})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.05);
+    cuff_beatI =  round(cuff_beatI); 
 end
 
 % Detect invasive aortic average 
 function invasive_average = calc_invasive_average(bigData, subject)
-    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).invasivedata{1,2}, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).invasivedata{1,2}), length(bigData(subject).invasivedata{1,2})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
-    invasive_average = analysis.AverageBeat(bigData(subject).invasivedata{1,2}', beatI, 1, 0); % '3' can include/excule good/bad data from average calculation, 0 for no plot, 1 for show plot 
+    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).filtered_invasivedata{1,1}, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).filtered_invasivedata{1,1}), length(bigData(subject).filtered_invasivedata{1,1})], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
+    invasive_average = analysis.AverageBeat(bigData(subject).filtered_invasivedata{1,1}', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, 0 for no plot, 1 for show plot 
 end
 
 % Detect invasive brachial average 
 function invasive_brachial_average = calc_invasive_brachial_average(bigData, subject)
-    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).invasivebrachialdata, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).invasivebrachialdata), length(bigData(subject).invasivebrachialdata)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
-    invasive_brachial_average = analysis.AverageBeat(bigData(subject).invasivebrachialdata', beatI, 1, 0); % '3' can include/excule good/bad data from average calculation, change to 0 for no plot
+    [beatI, ~] = analysis.BeatOnsetDetect(bigData(subject).filtered_invasivebrachialdata, 'Method', 'GradientIntersection', 'Interactive', 1,'RegionLimits', [max(bigData(subject).filtered_invasivebrachialdata), length(bigData(subject).filtered_invasivebrachialdata)], 'MinimumThreshold', 0.1, 'DerivativePeakThreshold', 0.55);
+    invasive_brachial_average = analysis.AverageBeat(bigData(subject).filtered_invasivebrachialdata', beatI, 1, 3); % '3' can include/excule good/bad data from average calculation, change to 0 for no plot
 end
+
