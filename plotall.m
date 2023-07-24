327d1ef79d788ca0148d11426c69ff4efbc1d911
corneyr <corneyr@utas.edu.au>
2023-07-24 14:15:35 +1000

    plotall.m

--- plotall.m
+++ plotall.m
@@ -1,12 +1,20 @@
-i = 26; %not done
-j = 1;
+i = 81; 
+j = 2;
 subplot (3,1,1)
 plot(bigData(i).filtered_cuffdata{1,(j)}); hold on; plot(bigData(i).detrend_cuffdata{1,(j)});
 legend({'Cuff filtered', 'Cuff unfiltered'});
 subplot (3,1,2)
 plot(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay')); hold on; plot(bigData(i).invasivedata{1,(j)});
+bigData(i).filtered_maxinvasiveindicies{j} = max(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay'));
+bigData(i).unfiltered_maxinvasiveindicies{j} = max(bigData(i).invasivedata{1,(j)});
+bigData(i).filtered_mininvasiveindicies{j} = min(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay'));
+bigData(i).unfiltered_mininvasiveindicies{j} = min(bigData(i).invasivedata{1,(j)});
 legend({'Aortic filtered', 'Aortic unfiltered'});
 subplot (3,1,3)
 plot(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay')); hold on; plot(bigData(i).invasivebrachialdata)
+bigData(i).filtered_maxinvasivebrachialindicies = max(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay'));
+bigData(i).unfiltered_maxinvasivebrachialindicies = max(bigData(i).invasivebrachialdata);
+bigData(i).filtered_mininvasivebrachialindicies = min(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay'));
+bigData(i).unfiltered_mininvasivebrachialindicies = min(bigData(i).invasivebrachialdata);
 legend({'Brachial filtered', 'Brachial unfiltered'});
 
