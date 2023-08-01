526f7040ddda7f64fb50e817868a0735f0286909
corneyr <corneyr@utas.edu.au>
2023-08-01 10:14:00 +1000

    compare_invasive_cuff.m

--- compare_invasive_cuff.m
+++ compare_invasive_cuff.m
@@ -1,52 +1,53 @@
 % input: bigData table from linkningxmltotxt.m
 % input: getCharacteristics_redcap.m
 % input: directory to data on device
-% 3 lots of j = x;
 
-for i = 1:length(bigData)
-    if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) 
-        continue
-    end 
 
+% for i = 1:length(bigData)
+%     if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) 
+%         disp(["No Data", i,j])
+%         continue 
+%     else 
+%         disp(["Figure created", i j])
+% 
+%     end 
+    i = 3;
     j = 1;
 
-
-
+figure();
 % plotting the aortic, brachial and cuff on one plot
 num_cols = 5;
-num_rows = ceil((length(bigData(i).cuff_beatI{1,j})-1)/num_cols)+1;
+num_rows = ceil((length(bigData(i).cuff_beatI{j})-1)/num_cols)+1;
 
-subplot(num_rows, num_cols, 1);
-[t, AI_aortic] = plot_aug_idx(bigData(i).invasive_average{1,j}, 0.001, false);
-t.String = "Aortic " + t.String;
+sp = subplot(num_rows, num_cols, 1); 
+title_handle = plot_aug_idx(bigData(i).invasive_average{j}, bigData(i).aortic_AI{j}, bigData(i).aortic_featuretimes{j}, 0.001, sp, false, false);
+title_handle.String = "Aortic " + title_handle.String;
 
 if ~isempty(bigData(i).invasivebrachialdata) 
-    subplot(num_rows, num_cols, num_cols);
-    [t, AI_brachial] = plot_aug_idx(bigData(i).invasivebrachial_average, 0.001, false);
-    t.String = "Brachial " + t.String;
+    sp = subplot(num_rows, num_cols, num_cols);
+    title_handle = plot_aug_idx(bigData(i).invasivebrachial_average, bigData(i).brachial_AI, bigData(i).brachial_featuretimes, 0.001, sp, false, false);
+    title_handle.String = "Brachial " + title_handle.String;
 end
 
-AIx = zeros(length(bigData(i).cuff_beatI{1,j})-1, 1);
-for n = 1:(length(bigData(i).cuff_beatI{1,j})-1)
-    subplot(num_rows, num_cols, num_cols+n);
-    time_offset = length(bigData(i).cuffdata{1,j}) - length(bigData(i).filtered_cuffdata{1,j});
-    pressure_offset = bigData(i).cuffdata{1,j}(bigData(i).cuff_beatI{1,j}(n)+time_offset);
-    data = bigData(i).filtered_cuffdata{1,j}(bigData(i).cuff_beatI{1,j}(n):bigData(i).cuff_beatI{1,j}(n+1)) + pressure_offset;
-    [~, AIx(n)] = plot_aug_idx(data, 0.005, true); 
+%cal
+time_offset = length(bigData(i).cuffdata{j}) - length(bigData(i).filtered_cuffdata{j});
+[~, min_idx] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}+time_offset) - bigData(i).aortic_sys{j}));
+
+for n = 1:(length(bigData(i).cuff_beatI{j})-1)
+    sp = subplot(num_rows, num_cols, num_cols+n);
+    time_offset = length(bigData(i).cuffdata{j}) - length(bigData(i).filtered_cuffdata{j});
+    pressure_offset = bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(n)+time_offset);
+    pulse = bigData(i).filtered_cuffdata{j}(bigData(i).cuff_beatI{j}(n):bigData(i).cuff_beatI{j}(n+1)) + pressure_offset;
+    plot_aug_idx(pulse, bigData(i).cuff_AIx{j}(n), bigData(i).cuff_featuretimes{j}{n}, 0.005, sp, true, n == min_idx); 
 end
 
-bigData(i).AI_aortic{j} = AI_aortic;
-bigData(i).AI_brachial = AI_brachial;
-bigData(i).AIx{j} = AIx;
 
-
-
-end %used for 'for i = 1:length(bigData)'
+ %used for 'for i = 1:length(bigData)'
 
 % Labelling the axis with name and AIx for each pulse
-function [t, AI] = plot_aug_idx(pulse, dt, subtract_flag)
+function title_handle = plot_aug_idx(pulse, AI, featuretimes, dt, sp, subtract_flag, bold_flag)
     time = (0:(length(pulse)-1))*dt;
-    [AI, ~, featuretimes] = analysis.AugmentationIndex(time, pulse, 'DoPlot',0);
+    
 
     if(subtract_flag) % detrend the cuff data
         start_pressure = pulse(1);
@@ -56,9 +57,10 @@ function [t, AI] = plot_aug_idx(pulse, dt, subtract_flag)
         line = slope * time;
         pulse = pulse - (line');
     end
-
-    plot(time, pulse);
-    t=0;
+    
+    weight = bold_flag+1;
+    plot(time, pulse, 'LineWidth', weight);
+    title_handle=0;
     if isstruct(featuretimes)
 
         x1 = xline(featuretimes.maxP,'-','Max P');
@@ -78,7 +80,8 @@ function [t, AI] = plot_aug_idx(pulse, dt, subtract_flag)
         x2.FontSize = 6;
         x3.FontSize = 6;
 
-        t = title(['AI = ', num2str(AI)]);
+        annotation('textbox','String',['AI = ', AI],'Position',sp.Position,'Vert','bottom','FitBoxToText','on')
+        title_handle = title(1);
     end
 
 end
