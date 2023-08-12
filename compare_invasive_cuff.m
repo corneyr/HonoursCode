1cd27ab94c955f985608e2510b3b5295b51d9615
corneyr <corneyr@utas.edu.au>
2023-08-13 09:37:10 +1000

    compare_invasive_cuff.m

--- compare_invasive_cuff.m
+++ compare_invasive_cuff.m
@@ -2,7 +2,6 @@
 % input: getCharacteristics_redcap.m
 % input: directory to data on device
 
-
 for i = 1:length(bigData)
     if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) 
         disp(["No Data", i,j])
@@ -11,54 +10,68 @@ for i = 1:length(bigData)
         disp(["Figure created", i j])
 
     end 
-    % i = 25;
-    j = 2;
-
-figure();
-% plotting the aortic, brachial and cuff on one plot
-num_cols = 5;
-num_rows = ceil((length(bigData(i).cuff_beatI{j})-1)/num_cols)+1;
-
-sp = subplot(num_rows, num_cols, 1); 
-title_handle = plot_aug_idx(bigData(i).invasive_average{j}, bigData(i).aortic_AI{j}, bigData(i).aortic_featuretimes{j}, 0.001, sp, "Aortic Pressure", false, false);
-
-if ~isempty(bigData(i).invasivebrachialdata) 
-    sp = subplot(num_rows, num_cols, num_cols);
-    title_handle = plot_aug_idx(bigData(i).invasivebrachial_average, bigData(i).brachial_AI, bigData(i).brachial_featuretimes, 0.001, sp, "Brachial Pressure", false, false);
-end
-
-%calulate idx of pulse whose start pressure is closest to aortic
-%systolic_pressure
-time_offset = length(bigData(i).cuffdata{j}) - length(bigData(i).filtered_cuffdata{j});
-[~, close_to_sys_idx] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}+time_offset) - bigData(i).aortic_sys{j}));
-[~, close_to_map_idx] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}+time_offset) - bigData(i).aortic_map{j}));
-[~, close_to_dia_idx] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}+time_offset) - bigData(i).aortic_dia{j}));
+    j = 1;
 
 
-lower_step_size = 1/(close_to_map_idx - close_to_sys_idx);
-upper_step_size = 1/(close_to_dia_idx - close_to_map_idx);
-
-for n = 1:(length(bigData(i).cuff_beatI{j})-1)
-    sp = subplot(num_rows, num_cols, num_cols+n);
+    % figure();
+    % plotting the aortic, brachial and cuff on one plot
+    num_cols = 5;
+    num_rows = ceil((length(bigData(i).cuff_beatI{j})-1)/num_cols)+1;
+    
+    sp = subplot(num_rows, num_cols, 1); 
+    title_handle = plot_aug_idx(bigData(i).invasive_average{j}, bigData(i).aortic_AI{j}, bigData(i).aortic_featuretimes{j}, 0.001, sp, "Aortic Pressure", false, false);
+    
+    if ~isempty(bigData(i).invasivebrachialdata) 
+        sp = subplot(num_rows, num_cols, num_cols);
+        title_handle = plot_aug_idx(bigData(i).invasivebrachial_average, bigData(i).brachial_AI, bigData(i).brachial_featuretimes, 0.001, sp, "Brachial Pressure", false, false);
+    end
+    
+    %calulate idx of pulse whose start pressure is closest to aortic
+    %systolic_pressure
     time_offset = length(bigData(i).cuffdata{j}) - length(bigData(i).filtered_cuffdata{j});
-    pressure_offset = bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(n)+time_offset);
-    filtered_pulse = bigData(i).filtered_cuffdata{j}(bigData(i).cuff_beatI{j}(n):bigData(i).cuff_beatI{j}(n+1));
-    pulse =  filtered_pulse - filtered_pulse(1) + pressure_offset;
-    if n == close_to_sys_idx || n == close_to_map_idx || n == close_to_dia_idx
-        bolded = true;
-    else
-        bolded = false;
-    end
+    [~, bigData(i).close_to_sys_idx{j}] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(1:end-1)+time_offset) - bigData(i).aortic_sys{j}));
+    [~, bigData(i).close_to_map_idx{j}] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(1:end-1)+time_offset) - bigData(i).aortic_map{j}));
+    [~, bigData(i).close_to_dia_idx{j}] = min(abs(bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(1:end-1)+time_offset) - bigData(i).aortic_dia{j}));
     
-    if n <= close_to_map_idx
-        pressure_phase_idx = -1 + lower_step_size*(n - close_to_sys_idx);
-    else 
-        pressure_phase_idx = upper_step_size*(n - close_to_map_idx);
-    end
-    
-    plot_aug_idx(pulse, bigData(i).cuff_AIx{j}(n), bigData(i).cuff_featuretimes{j}{n}, 0.005, sp, pressure_phase_idx, true, bolded); 
-end
+    %finding the AIx of the cuff beat found at the found aortic sys, map and dia
+    bigData(i).cuffAI_at_sys{j} = bigData(i).cuff_AIx{j}( bigData(i).close_to_sys_idx{j} );
+    bigData(i).cuffAI_at_map{j} = bigData(i).cuff_AIx{j}( bigData(i).close_to_map_idx{j} );
+    bigData(i).cuffAI_at_dia{j} = bigData(i).cuff_AIx{j}( bigData(i).close_to_dia_idx{j} );
 
+
+    lower_step_size = 1/(bigData(i).close_to_map_idx{j} - bigData(i).close_to_sys_idx{j});
+    upper_step_size = 1/(bigData(i).close_to_dia_idx{j} - bigData(i).close_to_map_idx{j});
+    
+    for n = 1:(length(bigData(i).cuff_beatI{j})-1)
+        sp = subplot(num_rows, num_cols, num_cols+n);
+        time_offset = length(bigData(i).cuffdata{j}) - length(bigData(i).filtered_cuffdata{j});
+        pressure_offset = bigData(i).cuffdata{j}(bigData(i).cuff_beatI{j}(n)+time_offset);
+        filtered_pulse = bigData(i).filtered_cuffdata{j}(bigData(i).cuff_beatI{j}(n):bigData(i).cuff_beatI{j}(n+1));
+        pulse =  filtered_pulse - filtered_pulse(1) + pressure_offset;
+        if n == bigData(i).close_to_sys_idx{j} || n == bigData(i).close_to_map_idx{j} || n == bigData(i).close_to_dia_idx{j}
+            bolded = true;
+        else
+            bolded = false;
+        end
+        
+        if n <= bigData(i).close_to_map_idx{j}
+            pressure_phase_idx = -1 + lower_step_size*(n - bigData(i).close_to_sys_idx{j});
+        else 
+            pressure_phase_idx = upper_step_size*(n - bigData(i).close_to_map_idx{j});
+        end
+        plot_aug_idx(pulse, bigData(i).cuff_AIx{j}(n), bigData(i).cuff_featuretimes{j}{n}, 0.005, sp, pressure_phase_idx, true, bolded); 
+         
+        % finding the cuff beat which has the closest AIx to the aortic AIx
+        [~, bigData(i).min_idx{j}] = min(abs(bigData(i).cuff_AIx{j} - bigData(i).aortic_AI{j}));
+        bigData(i).cuffidx_aorticAI_frommap{j} = (bigData(i).close_to_map_idx{j} - bigData(i).min_idx{j});
+        
+        %finding the amplification from aortic to brachial 
+        bigData(i).sbp_amp{j} = bigData(i).brachial_sys - bigData(i).aortic_sys{j};
+        bigData(i).dbp_amp{j} = bigData(i).brachial_dia - bigData(i).aortic_dia{j};
+        bigData(i).map_amp{j} = bigData(i).brachial_map - bigData(i).aortic_map{j};
+
+
+    end
 end %used for 'for i = 1:length(bigData)'
 
 % Labelling the axis with name and AIx for each pulse
@@ -99,6 +112,5 @@ function title_handle = plot_aug_idx(pulse, AI, featuretimes, dt, sp, title_str,
     new_limits = [floor(limits(1)), ceil(limits(2))];
     ylim(new_limits);
     yticks(new_limits);
-
-
+       
 end
