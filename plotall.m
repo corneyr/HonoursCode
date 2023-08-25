88f81c7d92851fe5fe315ffa7b2ce7da055ebc6e
corneyr <corneyr@utas.edu.au>
2023-08-25 12:18:26 +1000

    plotall.m

--- plotall.m
+++ plotall.m
@@ -1,26 +1,24 @@
-for i = 1:length(bigData)
-    if ~iscell(bigData(i).invasivedata) || ~iscell(bigData(i).cuffdata) 
-        continue
+for i = 7%1:length(filtvsunfilt)
+    if ~iscell(filtvsunfilt(i).invasivedata) || ~iscell(filtvsunfilt(i).cuffdata) 
+        disp(["No Data", i,j])
+        continue 
+    else 
+        disp(["Figure created", i j])
+
     end 
-        j = 2;
+
+        j = 1;
+        figure();
         subplot (3,1,1)
-        plot(smooth(bigData(i).detrend_cuffdata{j}, 10, 'sgolay')); hold on; plot(bigData(i).detrend_cuffdata{1,(j)});
+        plot(filtvsunfilt(i).filtered_cuffdata{1,(j)}); hold on; plot(filtvsunfilt(i).detrend_cuffdata{1,(j)});
         legend({'Cuff filtered', 'Cuff unfiltered'});
         
         subplot (3,1,2)
-        plot(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay')); hold on; plot(bigData(i).invasivedata{1,(j)});
-        bigData(i).filtered_maxinvasiveindicies{j} = max(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay'));
-        bigData(i).unfiltered_maxinvasiveindicies{j} = max(bigData(i).invasivedata{1,(j)});
-        bigData(i).filtered_mininvasiveindicies{j} = min(smooth(bigData(i).invasivedata{1,(j)}, 121, 'sgolay'));
-        bigData(i).unfiltered_mininvasiveindicies{j} = min(bigData(i).invasivedata{1,(j)});
+        plot(filtvsunfilt(i).filtered_invasivedata{1,(j)}); hold on; plot(filtvsunfilt(i).invasivedata{1,(j)});
         legend({'Aortic filtered', 'Aortic unfiltered'});
         
         subplot (3,1,3)
-        plot(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay')); hold on; plot(bigData(i).invasivebrachialdata)
-        bigData(i).filtered_maxinvasivebrachialindicies = max(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay'));
-        bigData(i).unfiltered_maxinvasivebrachialindicies = max(bigData(i).invasivebrachialdata);
-        bigData(i).filtered_mininvasivebrachialindicies = min(smooth(bigData(i).invasivebrachialdata, 121, 'sgolay'));
-        bigData(i).unfiltered_mininvasivebrachialindicies = min(bigData(i).invasivebrachialdata);
+        plot(filtvsunfilt(i).filtered_invasivebrachialdata); hold on; plot(filtvsunfilt(i).invasivebrachialdata)
         legend({'Brachial filtered', 'Brachial unfiltered'});
-
+    
 end 
